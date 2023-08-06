c62b61390e2b79037e6f805eb30c7102b474be1b
Esteban Sepulveda <tebansd@icloud.com>
2023-08-06 09:54:32 -0700

    minor fix

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const playGameBtn = document.getElementById("playGameBtn");
     
-    /* Add a 'click' event listener to the 'Play Game' button
-     This will navigate the user to the game.html page where the actual game is played */
+  /*  This will navigate the user to the game.html page where the actual game is played */
     playGameBtn.addEventListener("click", function () {
       window.location.href = "game.html";
     });
