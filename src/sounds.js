96976fdf60e3033f8829e5b5d3d793bc8a24c22d
Esteban Sepulveda <tebansd@icloud.com>
2024-04-01 17:13:40 -0700

    implemented mute

--- src/sounds.js
+++ src/sounds.js
@@ -1,8 +1,22 @@
 //Load sounds
 const racketSound = new Audio("../../assets/sounds/racketSound.mp3");
+let isMuted = true;
 
 // Function to play racket sound
 function playRacketSound() {
+  if (!isMuted) {
     racketSound.currentTime = 0;
-    racketSound.play();
+    racketSound
+      .play()
+      .catch((error) => console.error("Error playing sound:", error));
   }
+}
+
+document.getElementById("soundToggle").addEventListener("click", toggleSound);
+
+function toggleSound() {
+  isMuted = !isMuted;
+  document.getElementById("soundToggle").textContent = isMuted
+    ? "Unmute"
+    : "Mute";
+}
