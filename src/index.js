8be7e5a3521ae65b61378182b41336dcb192b960
Esteban Sepulveda <tebansd@icloud.com>
2024-04-02 17:10:11 -0700

    added credits modal

--- src/index.js
+++ src/index.js
@@ -2,17 +2,41 @@
 document.addEventListener("DOMContentLoaded", function () {
   const twoPlayersBtn = document.getElementById("twoPlayersBtn");
   const playCpuBtn = document.getElementById("playCpuBtn");
-  
+
   // Event listener for '2 Players' button
   twoPlayersBtn.addEventListener("click", function () {
-      localStorage.setItem('gameMode', '2P');
-      window.location.href = "game.html";
+    localStorage.setItem("gameMode", "2P");
+    window.location.href = "game.html";
   });
 
   // Event listener for 'Play CPU' button
   playCpuBtn.addEventListener("click", function () {
-      localStorage.setItem('gameMode', 'CPU');
-      window.location.href = "game.html";
+    localStorage.setItem("gameMode", "CPU");
+    window.location.href = "game.html";
   });
 });
 
+// Get the modal
+const modal = document.getElementById("creditsContainer");
+const btn = document.getElementById("creditsBtn");
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  document.getElementById("mainGame").style.display = "none";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+  document.getElementById("mainGame").style.display = "block";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    document.getElementById("mainGame").style.display = "block";
+  }
+};
